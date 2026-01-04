c2410dfe8b87f1b93ddb7a12ce5a535ad2d3244d
jackinfurs <jackinfurs@gmail.com>
2026-01-04 02:50:41 +0000

    cc: change web handle to jackinfurs.ie

--- cc.sh
+++ cc.sh
@@ -9,16 +9,16 @@ cat <<EOF
 
  ${dim}╭───────────────────────────────────────────────────────╮
  ${dim}│                                                       │
- ${dim}│${reset}          ${bold}Jack Dunne${reset}, fourth year cs student           ${dim}│
- ${dim}│${reset}             @ university college dublin${reset}               ${dim}│
+ ${dim}│${reset}           ${bold}Jack Dunne${reset}, final year cs student           ${dim}│
+ ${dim}│${reset}              @ university college dublin${reset}              ${dim}│
  ${dim}│                                                       │
- ${dim}│${reset}        ${bold}email:${reset}             jackinfurs${dim}@gmail.com        │
- ${dim}│${reset}        ${bold}web:${reset}               jackinfurs${dim}.github.io        │
+ ${dim}│${reset}        ${bold}web:${reset}                      jackinfurs.ie        │
+ ${dim}│${reset}        ${bold}email:${reset}                 me${dim}@jackinfurs.ie        │
  ${dim}│${reset}        ${bold}github:${reset}           ${dim}github.com/${reset}jackinfurs        ${dim}│
  ${dim}│${reset}        ${bold}codeberg:${reset}       ${dim}codeberg.org/${reset}jackinfurs        ${dim}│
  ${dim}│${reset}        ${bold}linkedin:${reset}    ${dim}linkedin.com/in/${reset}jackinfurs        ${dim}│
  ${dim}│                                                       │
- ${dim}│${reset}        ${bold}card:${reset}  ${yellow}curl -sL jackinfurs.github.io/cc${reset}        ${dim}│
+ ${dim}│${reset}        ${bold}card:${reset}         ${yellow}curl -sL jackinfurs.ie/cc${reset}        ${dim}│
  ${dim}│                                                       │
  ${dim}╰───────────────────────────────────────────────────────╯
 
